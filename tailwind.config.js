289a07c76bdf3e6e6bea9575ca9d82ef40ec9792
AbyssalVo1d <saullfisman97@gmail.com>
2025-07-18 18:19:27 -0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        abyssal: '#0D0F1C',
+        violet: '#7F5AF0',
+        blue: '#3C91E6'
+      }
+    },
+  },
+  plugins: [],
+}
